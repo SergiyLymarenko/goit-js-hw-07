d07bb429a10f978ada171346041cf3a3f04bed11
SergiyLymarenko <lymarenko.s.g@gmail.com>
2024-02-07 16:20:22 +0200

    second update

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  body: document.body,
+  span: document.querySelector(".color"),
+  btn: document.querySelector(".change-color"),
+}
+
+refs.btn.addEventListener("click", handleClick);
+function handleClick() {
+  const result = getRandomHexColor();
+  refs.span.textContent = result;
+  refs.body.style.backgroundColor = result;
+}
